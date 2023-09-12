81d19aafc6308a4947e728dd95b1c008a9b8b356
zowber <andy@zowber.com>
2023-09-12 14:37:26 +0100

    remove another unused typeface

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,7 @@
 import 'font-awesome/scss/font-awesome.scss'
 import './styles.scss'
 
-import { Open_Sans, Nunito_Sans } from 'next/font/google'
-
 import GoogleAnalytics from './GoogleAnalytics'
-
-const open_sans = Open_Sans({
-  subsets: ['latin'],
-  variable: '--font-open-sans',
-  display: 'swap',
-})
 
 export const metadata = {
   title: 'Andy Bright - Portfolio',
@@ -24,7 +16,6 @@ export default function RootLayout({
   return (
     <html
       lang='en'
-      className={`${open_sans.variable}`}
     >
       {process.env.GA_TRACKING_ID && (
         <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID} />
