f82e243a4fe167b2ca8e0ac8c98808a6f70b9e43
zowber <andy@zowber.com>
2023-09-12 14:16:02 +0100

    add GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,19 @@ import './styles.scss'
 
 import { Open_Sans, Nunito_Sans } from 'next/font/google'
 
-const open_sans = Open_Sans({ subsets: ['latin'], variable: '--font-open-sans', display: 'swap',})
-const nunito = Nunito_Sans({ weight: ['200', '300', '400', '600', '700', '800', '900'], subsets: ['latin'], variable: '--font-nunito-sans', display: 'swap',})
+import GoogleAnalytics from './GoogleAnalytics'
+
+const open_sans = Open_Sans({
+  subsets: ['latin'],
+  variable: '--font-open-sans',
+  display: 'swap',
+})
+const nunito = Nunito_Sans({
+  weight: ['200', '300', '400', '600', '700', '800', '900'],
+  subsets: ['latin'],
+  variable: '--font-nunito-sans',
+  display: 'swap',
+})
 
 export const metadata = {
   title: 'Zowber Portfolio',
@@ -17,7 +28,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${open_sans.variable} ${nunito.variable}`}>
+    <html
+      lang='en'
+      className={`${open_sans.variable} ${nunito.variable}`}
+    >
+      {process.env.GA_TRACKING_ID && (
+        <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID} />
+      )}
       <body>{children}</body>
     </html>
   )
